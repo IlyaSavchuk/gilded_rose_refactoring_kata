0695d476532741355964cd1fb0168fbcf81d4a25
Ilya <ilya.savchook@gmail.com>
2020-01-28 17:56:05 +0200

    Refactor gilded rose and add tests

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,50 +5,94 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      next if item.name == 'Sulfuras, Hand of Ragnaros'
+
+      decrease_sell_in(item)
+      update_item_quality(item)
     end
+  end
+
+  def update_item_quality(item)
+    case item.name
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      Backstage.new(item).update
+    when 'Aged Brie'
+      AgedBrie.new(item).update
+    when 'Conjured'
+      Conjured.new(item).update
+    else
+      UsualItem.new(item).update
+    end
+  end
+
+  def decrease_sell_in(item)
+    item.sell_in -= 1
+  end
+end
+
+class UsualItem
+  attr_accessor :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    decrease_quality(1) if item.quality < 50 && item.quality > 0
+  end
+
+  def decrease_quality(size)
+    return item.quality -= size * 2 if item.sell_in <= 0
+
+    item.quality -= size
+  end
+
+  def increase_quality
+    item.quality += 1
+  end
+end
+
+class AgedBrie < UsualItem
+  attr_accessor :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    increase_quality if item.quality < 50
+    increase_quality if expired?
+  end
+
+  def expired?
+    item.sell_in <= 0
+  end
+end
+
+class Backstage < UsualItem
+  attr_accessor :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    increase_quality if item.quality < 50
+    increase_quality if item.sell_in <= 10
+    increase_quality if item.sell_in <= 5
+    item.quality = 0 if item.sell_in <= 0
+  end
+end
+
+class Conjured < UsualItem
+  attr_accessor :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    decrease_quality(2)
   end
 end
 
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -2,10 +2,134 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
   describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+    let(:items) { [] }
+
+    context 'when item name is Sulfuras, Hand of Ragnaros' do
+      before do
+        items << Item.new('Sulfuras, Hand of Ragnaros', 2, 2)
+      end
+
+      it 'does not change the quality and sell_in' do
+        GildedRose.new(items).update_quality()
+        expect(items.first).to have_attributes(
+          sell_in: 2,
+          quality: 2
+        )
+      end
+    end
+
+    context 'when item name is Aged Brie' do
+      before do
+        items << Item.new('Aged Brie', 2, 2)
+      end
+
+      it 'changed the quality and sell_in' do
+        GildedRose.new(items).update_quality()
+        expect(items.first).to have_attributes(
+          sell_in: 1,
+          quality: 3
+        )
+      end
+    end
+
+    context 'when usual item' do
+      before do
+        items << Item.new('Usual item', 2, 2)
+      end
+
+      it 'changed the quality and sell_in' do
+        GildedRose.new(items).update_quality()
+        expect(items.first).to have_attributes(
+          sell_in: 1,
+          quality: 1
+        )
+      end
+    end
+
+    context 'when quality degrades twice as fast' do
+      before do
+        items << Item.new('Usual item', 0, 2)
+      end
+
+      it 'changed the quality and sell_in' do
+        GildedRose.new(items).update_quality()
+        expect(items.first).to have_attributes(
+          sell_in: -1,
+          quality: 0
+        )
+      end
+    end
+
+    context 'when quality of an item is never negative' do
+      before do
+        items << Item.new('Usual item', 0, 0)
+      end
+
+      it 'changed the quality and sell_in' do
+        GildedRose.new(items).update_quality()
+        expect(items.first).to have_attributes(
+          sell_in: -1,
+          quality: 0
+        )
+      end
+    end
+
+    context 'when item name is Backstage passes to a TAFKAL80ETC concert' do
+      context 'when sell_in is more 10 days' do
+        before do
+          items << Item.new('Backstage passes to a TAFKAL80ETC concert', 15, 2)
+        end
+
+        it 'changed the quality and sell_in' do
+          GildedRose.new(items).update_quality()
+          expect(items.first).to have_attributes(
+            sell_in: 14,
+            quality: 3
+          )
+        end
+      end
+
+      context 'when sell_in is less or equal 10 days' do
+        before do
+          items << Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 2)
+        end
+
+        it 'changed the quality and sell_in' do
+          GildedRose.new(items).update_quality()
+          expect(items.first).to have_attributes(
+            sell_in: 9,
+            quality: 4
+          )
+        end
+      end
+
+      context 'when sell_in is less or equal 5 days' do
+        before do
+          items << Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 2)
+        end
+
+        it 'changed the quality and sell_in' do
+          GildedRose.new(items).update_quality()
+          expect(items.first).to have_attributes(
+            sell_in: 4,
+            quality: 5
+          )
+        end
+      end
+
+      context 'when sell_in is 0 days' do
+        before do
+          items << Item.new('Backstage passes to a TAFKAL80ETC concert', 1, 2)
+        end
+
+        it 'changed the quality and sell_in' do
+          GildedRose.new(items).update_quality()
+          expect(items.first).to have_attributes(
+            sell_in: 0,
+            quality: 0
+          )
+        end
+      end
     end
   end
 end
